617efa403b27109bf1b5adad7db46971df9fb56d
Alberto <albertoavv@hotmail.es>
2024-08-04 23:58:51 -0400

    vue formularios 1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Formulario />
    <Boton nombre = "Enviar" disabled clase="success" />
    <Boton type="submit" nombre = "Eliminar" clase="danger" />
    <Boton type="submit" nombre = "Cuidado" clase="warning" />
@@ -9,6 +10,7 @@
 <script setup lang="ts">
 //import BotonContador from './components/BotonContador.vue';
 import Boton from './components/Boton.vue';
+import Formulario from './components/Formulario.vue';
 
 </script>
 
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,15 +1,25 @@
 <template>
     <div>
-
+        <input type="text" v-model="nombre">
+        <input type="number" v-model="edad">
+        <input type="date" v-model="fecha">
     </div>
+
+    <pre>
+        {{ nombre }} / {{ edad }} / {{ fecha }}
+    </pre>
 </template>
 
-<script>
-    export default {
-        
-    }
+<script setup lang="ts">
+import { ref } from "vue";
+
+const nombre = ref('')
+const edad = ref(0)
+const fecha = ref('')
+
+
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
